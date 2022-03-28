55e1a120d7a4e55c39cb57cf2ea58644298bf954
denis <182906@unsaac.edu.pe>
2022-03-28 10:22:38 -0500

    Add tailwindcss dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="text-red-900 text-4xl font-bold underline text-center">
+        Hello World!
+      </h1>
     </>
   );
 };
