bf7839f47593e07d758febc3018a819273a1c7a3
denis <182906@unsaac.edu.pe>
2022-03-28 12:35:36 -0500

    Add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import IconLuxury from "./assets/icon-luxury.svg";
 const App = () => {
   return (
     <>
-      <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex h-screen w-full items-center justify-center bg-[#F2F2F2]">
         <div className="h-[1323px] w-[325px] bg-black">
           {/*First card*/}
           <div className="h-1/3	bg-[#E38826]">
-            <div className="flex flex-col">
+            <div className="flex h-full flex-col justify-between py-6">
               {/*Icon sedans*/}
               <img
                 src={IconSedans}
@@ -18,53 +18,65 @@ const App = () => {
               />
 
               {/*Title*/}
-              <h1 className="text-white">SEDANS</h1>
+              <h1 className="font-bigShouldersDisplay text-[#F2F2F2]">
+                SEDANS
+              </h1>
 
               {/*Description*/}
-              <p className="text-white">
+              <p className="text-[#FFFFFF]">
                 Choose a sedan for its affordability and excellent fuel economy.
                 Ideal for cruising in the city or on your next road trip.
               </p>
 
               {/*Button*/}
-              <button>Learn more</button>
+              <button className="bg-[#F2F2F2]">Learn more</button>
             </div>
           </div>
 
           {/*Second card*/}
           <div className="h-1/3 bg-[#006970]">
-            {/*Icon suvs*/}
-            <img src={IconSuvs} alt="Suvs" className="h-[50px] w-[85px]" />
+            <div className="flex h-full flex-col justify-between py-6">
+              {/*Icon suvs*/}
+              <img src={IconSuvs} alt="Suvs" className="h-[50px] w-[85px]" />
 
-            {/*Title*/}
-            <h1 className="text-white">SUVS</h1>
+              {/*Title*/}
+              <h1 className="font-bigShouldersDisplay text-[#F2F2F2]">SUVS</h1>
 
-            {/*Description*/}
-            <p className="text-white">
-              Take an SUV for its spacious interior, power, and versatility.
-              Perfect for your next family vacation and off-road adventures.
-            </p>
+              {/*Description*/}
+              <p className="text-[#FFFFFF]">
+                Take an SUV for its spacious interior, power, and versatility.
+                Perfect for your next family vacation and off-road adventures.
+              </p>
 
-            {/*Button*/}
-            <button>Learn more</button>
+              {/*Button*/}
+              <button className="bg-[#F2F2F2]">Learn more</button>
+            </div>
           </div>
 
           {/*Third card*/}
           <div className="h-1/3 bg-[#004241]">
-            {/*Icon luxury*/}
-            <img src={IconLuxury} alt="Luxury" className="h-[50px] w-[85px]" />
+            <div className="flex h-full flex-col justify-between py-6">
+              {/*Icon luxury*/}
+              <img
+                src={IconLuxury}
+                alt="Luxury"
+                className="h-[50px] w-[85px]"
+              />
 
-            {/*Title*/}
-            <h1 className="text-white">LUXURY</h1>
+              {/*Title*/}
+              <h1 className="font-bigShouldersDisplay text-[#F2F2F2]">
+                LUXURY
+              </h1>
 
-            {/*Description*/}
-            <p className="text-white">
-              Cruise in the best car brands without the bloated prices. Enjoy
-              the enhanced comfort of a luxury rental and arrive in style.
-            </p>
+              {/*Description*/}
+              <p className="text-[#FFFFFF]">
+                Cruise in the best car brands without the bloated prices. Enjoy
+                the enhanced comfort of a luxury rental and arrive in style.
+              </p>
 
-            {/*Button*/}
-            <button>Learn more</button>
+              {/*Button*/}
+              <button className="bg-[#F2F2F2]">Learn more</button>
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      lexendDeca: ["Lexend Deca"],
+      bigShouldersDisplay: ["Big Shoulders Display"],
+    },
     extend: {},
   },
   plugins: [],
